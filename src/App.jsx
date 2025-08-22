8adffbabec717ff38ff96884c899ac3e3438bf9a
Arunkumar Madheswaran <43840802+amadheswaran@users.noreply.github.com>
2025-08-22 15:30:16 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import TweetScheduler from './components/TweetScheduler'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-blue-600 text-white p-4 text-xl font-semibold">
+        Tweet Scheduler
+      </header>
+      <main className="flex-1 p-4">
+        <TweetScheduler />
+      </main>
+      <footer className="bg-gray-200 p-4 text-center text-sm">
+        © 2025 TweetScheduler | Support | Settings
+      </footer>
+    </div>
+  )
+}
