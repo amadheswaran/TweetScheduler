14493bfd4dc7cb8f58bd2179fde246c910bc4042
Arunkumar Madheswaran <43840802+amadheswaran@users.noreply.github.com>
2025-08-23 19:54:19 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import TweetScheduler from './components/TweetScheduler'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-export default function App() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-blue-600 text-white p-4 text-xl font-semibold">
-        Tweet Scheduler
-      </header>
-      <main className="flex-1 p-4">
-        <TweetScheduler />
-      </main>
-      <footer className="bg-gray-200 p-4 text-center text-sm">
-        © 2025 TweetScheduler | Support | Settings
-      </footer>
-    </div>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
