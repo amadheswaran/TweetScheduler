134cf9ab4fc52f320c1c0d15ebfe2f71c03ff498
Arunkumar Madheswaran <43840802+amadheswaran@users.noreply.github.com>
2025-08-23 20:34:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import TweetScheduler from './components/TweetScheduler'
+import { useState } from "react";
+import TweetForm from "./components/TweetForm";
+import TweetList from "./components/TweetList";
+import TweetCalendar from "./components/TweetCalendar";
+import TweetAnalytics from "./components/TweetAnalytics";
+import DarkModeToggle from "./components/DarkModeToggle";
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+
+  const addTweet = (tweet) => {
+    setTweets([...tweets, tweet]);
+  };
+
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-blue-600 text-white p-4 text-xl font-semibold">
-        Tweet Scheduler
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      {/* Navbar */}
+      <header className="flex justify-between items-center px-6 py-4 bg-blue-600 text-white shadow-md">
+        <h1 className="text-xl font-bold">Tweet Scheduler</h1>
+        <DarkModeToggle />
       </header>
-      <main className="flex-1 p-4">
-        <TweetScheduler />
+
+      {/* Main Content */}
+      <main className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="col-span-1">
+          <TweetForm onAddTweet={addTweet} />
+        </div>
+        <div className="col-span-1">
+          <TweetList tweets={tweets} />
+        </div>
+        <div className="col-span-1 lg:col-span-2">
+          <TweetCalendar tweets={tweets} />
+        </div>
+        <div className="col-span-1 lg:col-span-3">
+          <TweetAnalytics tweets={tweets} />
+        </div>
       </main>
-      <footer className="bg-gray-200 p-4 text-center text-sm">
-        © 2025 TweetScheduler | Support | Settings
-      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
